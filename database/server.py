48b6e6ec834c8e04b3155e67a0f9a7ec048ed448
Guillermo Miranda <gdmn3000@gmail.com>
2023-08-03 22:29:47 -0400

    🔧Configuration: Agregar gitignore sugerido por github

--- database/server.py
+++ database/server.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
-from typing import Dict
+import typing
 import os
 import urllib.parse
 
@@ -9,24 +9,43 @@ load_dotenv()
 username = urllib.parse.quote_plus(os.getenv("USER"))
 password = urllib.parse.quote_plus(os.getenv("PASSWORD"))
 
-connection = MongoClient("mongodb+srv://%s:%s@cluster0.gjsvusw.mongodb.net/?retryWrites=true&w=majority" % (username, password))
+connection = MongoClient(
+    "mongodb+srv://%s:%s@cluster0.gjsvusw.mongodb.net/?retryWrites=true&w=majority" % (username, password))
 
 database = connection.dinoBaseDB
 collection = database.dinosaurs
+
+
+def aggregate():
+    document = collection.aggregate(
+        [
+            {
+                '$unset': '_id'
+            }, {
+                '$sample': {
+                    'size': 1
+                }
+            }
+        ])
+    return document.next()
+
 
 def delete_all():
     document = collection.delete_many({})
     return document.acknowledged
 
-def delete_many(data: Dict):
-    document = collection.delete_many(data)
-    return 
 
-def insert_many(data: Dict):
+def delete_many(data: typing.Dict):
+    document = collection.delete_many(data)
+    return document.acknowledged
+
+
+def insert_many(data: typing.Dict):
     document = collection.insert_many(data)
     return document.acknowledged
 
-def insert_one(data: Dict):
+
+def insert_one(data: typing.Dict):
     document = collection.insert_one(data)
     return document.acknowledged
 
